196a4d86e4c90b8b24ef411a8d724f2c447b97df
YakoViTo <kavirjosegermanav@gmail.com>
2023-06-09 15:56:59 -0400

    añadí unos campos fechas

--- GenerarPrestamo/models.py
+++ GenerarPrestamo/models.py
@@ -8,6 +8,7 @@ class Estudiante(models.Model):
     direccion=models.CharField(max_length=50)
     email=models.EmailField(blank=True, null=True)
     tfn=models.CharField(max_length=15)
+    fecha=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.nombre
@@ -17,10 +18,11 @@ class Libro(models.Model):
     nombre=models.CharField(max_length=30)
     autor=models.CharField(max_length=30)
     seccion=models.CharField(max_length=20)
+    fecha=models.DateTimeField(auto_now_add=True)
 
 class Prestamo(models.Model):
     numero=models.IntegerField()
     codigo=models.CharField(max_length=20)
-    fecha=models.DateField()
-    limite=models.DateField()
+    fecha=models.DateTimeField(auto_now_add=True)
+    limite=models.DateTimeField(verbose_name="Fecha límite")
     nombre=models.CharField(max_length=30, verbose_name="Nombre del estudiante")
