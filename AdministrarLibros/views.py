c658eb413f27fca21ffd6af57ae50a20a6ffed54
YakoViTo <kavirjosegermanav@gmail.com>
2023-06-16 14:06:17 -0400

    creando vistas

--- AdministrarLibros/views.py
+++ AdministrarLibros/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def administrar_libros(request):
+
+    return render(request, "administrarlibros/administrarlibros.html")
--- GenerarPrestamo/views.py
+++ GenerarPrestamo/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def generar_prestamo(request):
+
+    return render(request, "generarprestamo/generarprestamo.html")
