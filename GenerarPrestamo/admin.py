9cef3bbb3eedeb38aca1fba57ad13f84b6e8b5ec
YakoViTo <kavirjosegermanav@gmail.com>
2023-06-16 07:59:18 -0400

    reacomodando bdd

--- GenerarPrestamo/admin.py
+++ GenerarPrestamo/admin.py
@@ -1,23 +1,12 @@
 from django.contrib import admin
-from GenerarPrestamo.models import Estudiante, Libro, Prestamo
+from GenerarPrestamo.models import Prestamo
 
 # Register your models here.
-class EstudiantesAdmin(admin.ModelAdmin):
-    list_display=("cedula", "nombre", "direccion", "email", "tfn", "fecha")
-    search_fields=("cedula", "nombre", "direccion", "email", "tfn", "fecha")
-    list_filter=("nombre", "fecha")
-
-class LibrosAdmin(admin.ModelAdmin):
-    list_display=("codigo", "nombre", "autor", "seccion", "fecha")
-    search_fields=("codigo", "nombre", "autor", "seccion", "fecha")
-    list_filter=("seccion", "autor", "fecha")
 
 class PrestamosAdmin(admin.ModelAdmin):
-    list_display=("numero", "codigo", "fecha", "limite", "nombre")
-    search_fields=("numero", "codigo", "nombre")
+    list_display=("numero", "codigo", "fecha", "limite", "cedula")
+    search_fields=("numero", "codigo", "cedula")
     list_filter=("fecha", "limite")
     date_hierarchy=("limite")
 
-admin.site.register(Estudiante, EstudiantesAdmin)
-admin.site.register(Libro, LibrosAdmin)
 admin.site.register(Prestamo, PrestamosAdmin)
--- GenerarPrestamo/models.py
+++ GenerarPrestamo/models.py
@@ -2,36 +2,14 @@ from django.db import models
 
 # Create your models here.
 
-class Estudiante(models.Model):
-    cedula=models.CharField(max_length=30)
-    nombre=models.CharField(max_length=30)
-    direccion=models.CharField(max_length=50)
-    email=models.EmailField(blank=True, null=True)
-    tfn=models.CharField(max_length=15)
-    fecha=models.DateField(auto_now_add=True)
-
-    class Meta:
-        verbose_name='estudiante'
-        verbose_name_plural='estudiantes'
-
-class Libro(models.Model):
-    codigo=models.CharField(max_length=20)
-    nombre=models.CharField(max_length=30)
-    autor=models.CharField(max_length=30)
-    seccion=models.CharField(max_length=20)
-    fecha=models.DateField(auto_now_add=True)
-
-    class Meta:
-        verbose_name='libro'
-        verbose_name_plural='libros'
-
 class Prestamo(models.Model):
     numero=models.IntegerField()
     codigo=models.CharField(max_length=20)
     fecha=models.DateField(auto_now_add=True)
     limite=models.DateField(default='')
-    nombre=models.CharField(max_length=30, verbose_name="Nombre del estudiante")
+    cedula=models.IntegerField(verbose_name="CI del Estudiante")
 
     class Meta:
         verbose_name='prestamo'
         verbose_name_plural='prestamos'
+
