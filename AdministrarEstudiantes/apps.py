65d8f12c4daffddcd91f988c27d9d7f92114f0b4
YakoViTo <kavirjosegermanav@gmail.com>
2023-06-16 10:15:39 -0400

    organizando apps

--- AdministrarEstudiantes/apps.py
+++ AdministrarEstudiantes/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class VisualizarprestamosConfig(AppConfig):
+class AdministrarestudiantesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'VisualizarPrestamos'
+    name = 'AdministrarEstudiantes'
---
+++ AdministrarLibros/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AdministrarlibrosConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'AdministrarLibros'
