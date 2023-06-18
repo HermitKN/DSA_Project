2d0783809c0c9a2ab4499b2235a0f88923c71119
HermitKN <eduardo.420k@gmail.com>
2023-06-18 18:43:45 -0400

    add: change hermit

--- Home/urls.py
+++ Home/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('',views.home, name="Home"),
     path('notas',views.notas_prestamo, name="NotasPrestamo"),
     path('libros',views.administrar_libros, name="Libros"),
-    path('libros',views.administrar_estudiantes, name="Estudiantes"),
+    path('estudiantes',views.administrar_estudiantes, name="Estudiantes"),
 ]
