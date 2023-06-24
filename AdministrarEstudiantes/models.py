1b2befca08cc0562ecfcdef3f19cddfc365c8a17
YakoViTo <135473233+YakoViTo@users.noreply.github.com>
2023-06-24 08:58:48 -0400

    Update models.py
    Adding comment

--- AdministrarEstudiantes/models.py
+++ AdministrarEstudiantes/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Create your models here.
 
-class Estudiante(models.Model):
-    
+class Estudiante(models.Model):# Creación de la tabla con el nombre Estudiante
+    # Campos de la tabla
     nombre=models.CharField(max_length=20, verbose_name="Nombre del Estudiante")
     cedula=models.CharField(max_length=20, verbose_name="CI del Estudiante")
     email=models.EmailField(max_length=20, verbose_name="Correo Electrónico")
     direccion=models.CharField(max_length=50, verbose_name="Dirección del Estudiante")
     tfno=models.CharField(max_length=20, verbose_name="Número Telefónico")
-    fecha=models.DateTimeField(auto_now_add=True, verbose_name="Fecha del Registro")
+    fecha=models.DateTimeField(auto_now_add=True, verbose_name="Fecha del Registro")# En este campo, con el auto_now_add se permitirá que la información del tiempo se genere automáticamente, por lo tanto no contará para el formulario al momento de agregar un estudiante
 
     def __str__(self):
         return self.nombre
