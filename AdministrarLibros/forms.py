8aa83b653dd3e8c2a07dc6971a87649df3fcedb5
YakoViTo <kavirjosegermanav@gmail.com>
2023-06-25 01:31:09 -0400

    kek

--- AdministrarLibros/forms.py
+++ AdministrarLibros/forms.py
@@ -12,5 +12,5 @@ class FormularioLibro(forms.Form): #Creación de formulario con el nombre Formul
     nombre=forms.CharField(label="Nombre del Libro", max_length=50, required=True)
     autor=forms.CharField(label="Autor", max_length=50, required=True)
     tipo=forms.ChoiceField(label="Tipo", choices=Choice_Libro)
-    categoria=forms.CharField(label="Tags", max_length=20, required=True)
     cantidad = forms.IntegerField(label="Numero de Copias",required=True)
+    categoria=forms.CharField(label="Tags", max_length=20, required=True)
--- AdministrarLibros/models.py
+++ AdministrarLibros/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Libro(models.Model):# Tabla con el nombre Libro
 
     nombre=models.CharField(max_length=50, verbose_name="Nombre del Libro")
-    id=models.CharField(max_length=20, verbose_name="ID del Libro", primary_key=True)
+    id=models.CharField(max_length=30, verbose_name="ID del Libro", primary_key=True)
     autor=models.CharField(max_length=50, verbose_name="Autor del libro")
     tipo=models.CharField(max_length=15, verbose_name="Autor del libro")
     cantidad=models.IntegerField(verbose_name="Cantidad de copias")
