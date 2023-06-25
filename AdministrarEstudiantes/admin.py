b4e26a15cf7715c36bf330a1f23f240c8a640ca4
YakoViTo <kavirjosegermanav@gmail.com>
2023-06-25 14:49:58 -0400

    kek

--- AdministrarEstudiantes/admin.py
+++ AdministrarEstudiantes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Estudiante
 
 # Register your models here.
+
+class EstudiantesAdmin(admin.ModelAdmin):
+    list_display=("id", "nombre", "email", "direccion", "tfno", "fecha")
+    search_fields=("id", "nombre", "email", "direccion", "tfno")
+    list_filter=("nombre", "fecha", "direccion")
+    date_hierarchy=("fecha")
+
+admin.site.register(Estudiante, EstudiantesAdmin)
--- AdministrarLibros/admin.py
+++ AdministrarLibros/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Libro
 
 # Register your models here.
+
+class LibrosAdmin(admin.ModelAdmin):
+    list_display=("id", "nombre", "autor", "tipo", "cantidad", "categoria", "fecha")
+    search_fields=("id", "nombre", "autor", "tipo", "cantidad", "categoria")
+    list_filter=("nombre", "autor", "tipo", "categoria")
+    date_hierarchy=("fecha")
+
+admin.site.register(Libro, LibrosAdmin)
