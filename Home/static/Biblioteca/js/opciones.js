02436cc85f0a49c6b5d617089a48ed2f926a7898
HermitKN <eduardo.420k@gmail.com>
2023-07-01 02:38:18 -0400

    add: hermit

---
+++ Home/static/Biblioteca/js/opciones.js
@@ -0,0 +1,29 @@
+//Condiciones para los divs de administrarlibros
+
+function MostrarIngresar(){
+    document.getElementById("Ingresar").style.display = "block"
+    document.getElementById("Visualizar").style.display = "none"
+    document.getElementById("Eliminar").style.display = "none"
+    document.getElementById("Editar").style.display = "none"
+}
+
+function MostrarVisualizar(){
+    document.getElementById("Ingresar").style.display = "none"
+    document.getElementById("Visualizar").style.display = "block"
+    document.getElementById("Eliminar").style.display = "none"
+    document.getElementById("Editar").style.display = "none"
+}
+
+function MostrarEliminar(){
+    document.getElementById("Ingresar").style.display = "none"
+    document.getElementById("Visualizar").style.display = "none"
+    document.getElementById("Eliminar").style.display = "block"
+    document.getElementById("Editar").style.display = "none"
+}
+
+function MostrarEditar(){
+    document.getElementById("Ingresar").style.display = "none"
+    document.getElementById("Visualizar").style.display = "none"
+    document.getElementById("Eliminar").style.display = "none"
+    document.getElementById("Editar").style.display = "block"
+}
