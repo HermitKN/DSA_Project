5a4f33d5ff690d5e25cd79193d725b13a690f86d
YakoViTo <kavirjosegermanav@gmail.com>
2023-07-09 21:14:13 -0400

    kek

--- NotasPrestamo/urls.py
+++ NotasPrestamo/urls.py
@@ -4,6 +4,4 @@ from .import views
 urlpatterns = [ 
 
     path('',views.notas_prestamo, name="NotasPrestamo"),
-    #path('cambiarpenalizacion/<int:prestamo_id>/', views.cambiar_penalizacion, name='CambiarPenalizacion'),
-    #path('editarcantidaddevuelta/<int:prestamo_id>/', views.editar_cantidad_devuelta, name='EditarCantidadDevuelta'),
 ]
