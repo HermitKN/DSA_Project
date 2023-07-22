d47a400370f85222e84ce1c4fca94ef266fba2b4
HermitKN <eduardo.420k@gmail.com>
2023-07-21 21:07:22 -0400

    add: hermit

--- NotasPrestamo/views.py
+++ NotasPrestamo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from GenerarPrestamo.models import Prestamo
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -25,8 +26,28 @@ def notas_prestamo(request):
             libro.save()
             return redirect('/notasprestamo/?true')
     
+    # Filtros
+    mostrar_penalizados = request.GET.get('penalizados')
+    mostrar_no_penalizados = request.GET.get('no_penalizados')
+    
     notas=Prestamo.objects.all()
-    return render(request, "notasprestamo/notasprestamo.html", {'notas':notas})
+    
+    # Filtrar por préstamos con estudiantes penalizados o no penalizados si se especifica el parámetro en la URL
+    if mostrar_penalizados is not None:
+        notas = notas.filter(penalizacion__exact='Penalización semanal')
+        
+    if mostrar_no_penalizados is not None:
+        notas = notas.filter(penalizacion__exact='Sin penalización')
+    
+    # Definir el número de elementos por página
+    elementos_por_pagina = 5
+    paginator = Paginator(notas, elementos_por_pagina)
+
+    # Obtener el número de página desde la consulta GET, si no se especifica, será la página 1
+    pagina_numero = request.GET.get('pagina')
+    pagina_obj = paginator.get_page(pagina_numero)
+    
+    return render(request, "notasprestamo/notasprestamo.html", {'notas':notas, 'pagina_obj': pagina_obj})
 
 
 
